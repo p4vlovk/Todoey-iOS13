895d9065bdf96eb28a4648f710edb1490451515b
Kaloyan Pavlov <kaloyan.pavlov@Tests-Mac-mini.local>
2021-04-06 13:15:20 +0300

    Create table view and related behavior

---
+++ Todoey/Constants.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct K {
+    static let cellIdentifier = "ToDoItemCell"
+    static let alertTitle = "Add New Todoey Item"
+    static let alertActionTitle = "Add Item"
+    static let alertTextFieldPlaceholder = "Create new item"
+}
